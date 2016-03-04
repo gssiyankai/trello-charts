2bc0cdca6cacc5a84ff3d3f01877b76cffb2df51
gssiyankai <syk.greg@gmail.com>
2016-03-04 17:04:38 +0100

    Extract settings properties

---
+++ src/main/java/com/gregory/Settings.java
@@ -0,0 +1,46 @@
+package com.gregory;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public final class Settings {
+
+    private static final String SETTINGS_PROPERTIES = "settings.properties";
+    private static final String TRELLO_KEY = "trelloKey";
+    private static final String TRELLO_ACCESS_TOKEN = "trelloAccessToken";
+    private static final String TRELLO_BOARD_ID = "trelloBoardId";
+
+    private static Settings SINGLETON;
+
+    private final String trelloKey;
+    private final String trelloAccessToken;
+    private final String trelloBoardId;
+
+    private Settings() throws IOException {
+        Properties properties = new Properties();
+        properties.load(this.getClass().getResourceAsStream("/" + SETTINGS_PROPERTIES));
+        this.trelloKey = properties.getProperty(TRELLO_KEY);
+        this.trelloAccessToken = properties.getProperty(TRELLO_ACCESS_TOKEN);
+        this.trelloBoardId = properties.getProperty(TRELLO_BOARD_ID);
+    }
+
+    public static Settings settings() throws IOException {
+        if (SINGLETON == null) {
+            SINGLETON = new Settings();
+        }
+        return SINGLETON;
+    }
+
+    public String trelloKey() {
+        return trelloKey;
+    }
+
+    public String trelloAccessToken() {
+        return trelloAccessToken;
+    }
+
+    public String trelloBoardId() {
+        return trelloBoardId;
+    }
+
+}
--- src/main/java/com/gregory/TrelloCharts.java
+++ src/main/java/com/gregory/TrelloCharts.java
@@ -1,38 +1,23 @@
 package com.gregory;
 
 import com.julienvey.trello.Trello;
-import com.julienvey.trello.domain.Board;
-import com.julienvey.trello.domain.TList;
+import com.julienvey.trello.domain.Card;
 import com.julienvey.trello.impl.TrelloImpl;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Properties;
+
+import static com.gregory.Settings.settings;
 
 public final class TrelloCharts {
 
-    private static final String SETTINGS_PROPERTIES = "settings.properties";
-    private static final String TRELLO_KEY = "trelloKey";
-    private static final String TRELLO_ACCESS_TOKEN = "trelloAccessToken";
-    private static final String TRELLO_BOARD_ID = "trelloBoardId";
-
-    private final String trelloKey;
-    private final String trelloAccessToken;
-    private final String trelloBoardId;
+    private final Trello trello;
 
     public TrelloCharts() throws IOException {
-        Properties properties = new Properties();
-        properties.load(this.getClass().getResourceAsStream("/" + SETTINGS_PROPERTIES));
-        this.trelloKey = properties.getProperty(TRELLO_KEY);
-        this.trelloAccessToken = properties.getProperty(TRELLO_ACCESS_TOKEN);
-        this.trelloBoardId = properties.getProperty(TRELLO_BOARD_ID);
+        trello = new TrelloImpl(settings().trelloKey(), settings().trelloAccessToken());
     }
 
-    public void generateBurndownChart() {
-        Trello trelloApi = new TrelloImpl(trelloKey, trelloAccessToken);
-        Board board = trelloApi.getBoard(trelloBoardId);
-        List<TList> lists = board.fetchLists();
-        System.out.println(lists);
+    public void generateBurndownChart() throws IOException {
+
     }
 
     public static void main(String[] args) throws Exception {
