225eb252523bb0b9058e9c635bf4a0abb310dd40
gssiyankai <syk.greg@gmail.com>
2016-03-04 14:29:15 +0100

    Fix keys

--- src/main/java/com/gregory/TrelloCharts.java
+++ src/main/java/com/gregory/TrelloCharts.java
@@ -14,23 +14,23 @@ public final class TrelloCharts {
     private static final String SETTINGS_PROPERTIES = "settings.properties";
     private static final String TRELLO_KEY = "trelloKey";
     private static final String TRELLO_ACCESS_TOKEN = "trelloAccessToken";
-    private static final String TRELLO_BOARD_FOR_ADDING_CARDS_ID = "trelloBoardForAddingCardsId";
+    private static final String TRELLO_BOARD_ID = "trelloBoardId";
 
     private final String trelloKey;
     private final String trelloAccessToken;
-    private final String trelloBoardForAddingCardsId;
+    private final String trelloBoardId;
 
     public TrelloCharts() throws IOException {
         Properties properties = new Properties();
         properties.load(this.getClass().getResourceAsStream("/" + SETTINGS_PROPERTIES));
         this.trelloKey = properties.getProperty(TRELLO_KEY);
         this.trelloAccessToken = properties.getProperty(TRELLO_ACCESS_TOKEN);
-        this.trelloBoardForAddingCardsId = properties.getProperty(TRELLO_BOARD_FOR_ADDING_CARDS_ID);
+        this.trelloBoardId = properties.getProperty(TRELLO_BOARD_ID);
     }
 
     public void generateBurndownChart() {
         Trello trelloApi = new TrelloImpl(trelloKey, trelloAccessToken);
-        Board board = trelloApi.getBoard(trelloBoardForAddingCardsId);
+        Board board = trelloApi.getBoard(trelloBoardId);
         List<TList> lists = board.fetchLists();
         System.out.println(lists);
     }
